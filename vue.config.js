167c1a69c135059b4fb371399b485607012fd06f
chtzs <wsbcht@sina.com>
2022-06-24 14:55:26 +0800

    完善

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
     devServer: {
-        disableHostCheck: true,
+        allowedHosts: "all",
         port: 8081
     }
 })
